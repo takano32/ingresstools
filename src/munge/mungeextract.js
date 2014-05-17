e81fa4ecfa53a1ac59d9bb8ad11d92390a406564
Akira KUMAGAI <kuma@ultrasync.net>
2014-05-18 05:15:56 +0900

    set UserAgent

--- src/munge/mungeextract.js
+++ src/munge/mungeextract.js
@@ -5,6 +5,9 @@
 var page = require('webpage').create();
 var fs = require('fs');
 
+page.customHeaders = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.99 Safari/537.36"
+};
 
 page.onInitialized = function(){
     page.evaluate(function(){
